fa638c96ef2be91fe4644afe85d8876789102bd8
Jose.Nortes@gft.com <Jose.Nortes@gft.com>
2023-03-17 13:00:11 +0100

    feat: message service implemented in reactive mode

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,55 +1,54 @@
-import { CoursesService } from './../services/courses.service';
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import { LoadingService } from '../loading/loading-service';
-
+import { CoursesService } from "./../services/courses.service";
+import { Component, OnInit } from "@angular/core";
+import { Course, sortCoursesBySeqNo } from "../model/course";
+import { Observable, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { LoadingService } from "../loading/loading-service";
+import { MessagesService } from "../messages/messages.service";
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
   beginnerCourses$: Observable<Course[]>;
 
   advancedCourses$: Observable<Course[]>;
 
-
-  constructor(private coursesService: CoursesService, private loadingService: LoadingService) {
-
-  }
+  constructor(
+    private coursesService: CoursesService,
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
+  ) {}
 
   ngOnInit() {
-
     this.reloadCourses();
   }
 
-  reloadCourses(){
-    const courses$ = this.coursesService.loadAllCourses()
-    .pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo) )
+  reloadCourses() {
+    const courses$ = this.coursesService.loadAllCourses().pipe(
+      map((courses) => courses.sort(sortCoursesBySeqNo)),
+      catchError((err) => {
+        const message = "Could not load courses";
+        this.messagesService.showErrors(message);
+        console.log(message, err);
+        return throwError(err);
+      })
     );
 
-    const loadingCourses$ = this.loadingService.showUntilComplete(courses$)
+    const loadingCourses$ = this.loadingService.showUntilComplete(courses$);
 
-    this.beginnerCourses$ = loadingCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category === "BEGINNER"))
-      );
+    this.beginnerCourses$ = loadingCourses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "BEGINNER")
+      )
+    );
 
-    this.advancedCourses$ = loadingCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category === "ADVANCED"))
-      );
+    this.advancedCourses$ = loadingCourses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "ADVANCED")
+      )
+    );
   }
-
-
-
 }
-
-
-
-
