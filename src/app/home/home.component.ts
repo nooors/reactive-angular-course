ac7306eae933c1b647b910bf11d60cc3cf8dfdc1
Jose Nortes <nooors@gmail.com>
2023-03-18 09:44:59 +0100

    fix: unused imports removed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,8 @@
 import { CoursesService } from './../services/courses.service';
 import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 
 @Component({
